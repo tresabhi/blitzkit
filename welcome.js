ddede49160bbf9e074f6c7a2c3475bcfa4d6d17c
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-18 01:33:01 +0000

    feat: faker js

--- welcome.js
+++ welcome.js
@@ -1,18 +1,19 @@
+const { faker } = require("@faker-js/faker");
+
 const CHAT_CHANNEL = "1097677884266643527";
 const RULES_CHANNEL = "912486823522095114";
 
 module.exports = (client) => {
   client.on("guildMemberAdd", (member) => {
-    console.log(member);
-
-    // const message = `Welcome <@${
-    //   member.id
-    // }> to Skilled! Be sure to check out our ${member.guild.channels.cache
-    //   .get(RULES_CHANNEL)
-    //   .toString()}`;
-    const message = `Welcome <@${member.id}> to Skilled!`;
-
-    const channel = member.guild.channels.cache.get(CHAT_CHANNEL);
-    channel.send(message);
+    member.guild.channels.cache
+      .get(CHAT_CHANNEL)
+      .send(
+        `Welcome <@${
+          member.id
+        }> to Skilled! Use \`/verify <your in-game name>\` to continue.\nExample: \`/verify ${faker.internet.userName(
+          undefined,
+          ""
+        )}\` (capitalization matters!)`
+      );
   });
 };
