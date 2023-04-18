8462901ef7e754a7b5efcdefba95a402add70e08
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2023-04-17 19:02:53 -0500

    Initial commit

---
+++ src/commands/args.ts
@@ -0,0 +1,51 @@
+import { APIApplicationCommandBasicOption, ApplicationCommandOptionType } from 'discord.js';
+
+import { HelpOption, InfoOption } from '../enums/index.js';
+import { Language } from '../models/enum-helpers/index.js';
+import { Lang } from '../services/index.js';
+
+export class Args {
+    public static readonly HELP_OPTION: APIApplicationCommandBasicOption = {
+        name: Lang.getRef('arguments.option', Language.Default),
+        name_localizations: Lang.getRefLocalizationMap('arguments.option'),
+        description: Lang.getRef('argDescs.helpOption', Language.Default),
+        description_localizations: Lang.getRefLocalizationMap('argDescs.helpOption'),
+        type: ApplicationCommandOptionType.String,
+        choices: [
+            {
+                name: Lang.getRef('helpOptionDescs.contactSupport', Language.Default),
+                name_localizations: Lang.getRefLocalizationMap('helpOptionDescs.contactSupport'),
+                value: HelpOption.CONTACT_SUPPORT,
+            },
+            {
+                name: Lang.getRef('helpOptionDescs.commands', Language.Default),
+                name_localizations: Lang.getRefLocalizationMap('helpOptionDescs.commands'),
+                value: HelpOption.COMMANDS,
+            },
+        ],
+    };
+    public static readonly INFO_OPTION: APIApplicationCommandBasicOption = {
+        name: Lang.getRef('arguments.option', Language.Default),
+        name_localizations: Lang.getRefLocalizationMap('arguments.option'),
+        description: Lang.getRef('argDescs.helpOption', Language.Default),
+        description_localizations: Lang.getRefLocalizationMap('argDescs.helpOption'),
+        type: ApplicationCommandOptionType.String,
+        choices: [
+            {
+                name: Lang.getRef('infoOptions.about', Language.Default),
+                name_localizations: Lang.getRefLocalizationMap('infoOptions.about'),
+                value: InfoOption.ABOUT,
+            },
+            {
+                name: Lang.getRef('infoOptions.translate', Language.Default),
+                name_localizations: Lang.getRefLocalizationMap('infoOptions.translate'),
+                value: InfoOption.TRANSLATE,
+            },
+            {
+                name: Lang.getRef('infoOptions.dev', Language.Default),
+                name_localizations: Lang.getRefLocalizationMap('infoOptions.dev'),
+                value: InfoOption.DEV,
+            },
+        ],
+    };
+}
