2e60df7776a32da5411409174f78b0a304466a33
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-18 22:53:48 +0000

    feat: new welcome prompt

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+import { faker } from '@faker-js/faker';
+import { Client, EmbedBuilder, GatewayIntentBits } from 'discord.js';
+import config from '../config.json' assert { type: 'json' };
 
-const config = require("../config.json");
-const welcome = require("./welcome");
+const VERIFY_CHANNEL = '1097677884266643527';
 
-client.on("ready", () => {
-  console.log("Skilled Bot is now online");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
 
-  welcome(client);
+client.on('error', console.error);
+client.on('ready', (c) => console.log(`Logged in as ${client.user.tag}!`));
+
+client.on('guildMemberAdd', (member) => {
+  member.guild.channels.cache.get(VERIFY_CHANNEL).send({
+    embeds: [
+      new EmbedBuilder()
+        .setColor('#8e3cf5')
+        .setTitle(`Welcome ${member.user.username}`)
+        .setDescription(
+          `Welcome to the Skilled server, ${member.toString()}! To continue, please use the \`/verify\` command.\n\nExample: \`/verify ${faker.name.firstName()}${faker.datatype.number(
+            { min: 1, max: 100 },
+          )}\``,
+        ),
+    ],
+  });
 });
 
 client.login(config.token);
