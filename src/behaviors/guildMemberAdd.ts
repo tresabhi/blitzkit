e2debc5a256f626d1cbfad13a544377469efe686
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-23 00:59:08 +0000

    feat: removed faker.js

--- src/behaviors/guildMemberAdd.ts
+++ src/behaviors/guildMemberAdd.ts
@@ -1,4 +1,3 @@
-import { faker } from '@faker-js/faker';
 import { EmbedBuilder, GuildMember, TextChannel } from 'discord.js';
 import discord from '../../discord.json' assert { type: 'json' };
 import { SKILLED_COLOR } from '../constants/colors.js';
@@ -15,11 +14,7 @@ export default async function (member: GuildMember) {
         .setColor(SKILLED_COLOR)
         .setTitle(`Welcome ${member.user.username}`)
         .setDescription(
-          `Welcome to the Skilled server, **${
-            member.user.username
-          }**! To continue, please use the \`/verify\` command.\n\nExample: \`/verify NA ${faker.name.firstName()}${faker.datatype.number(
-            { min: 1, max: 100 },
-          )}\``,
+          `Welcome to the Skilled server, **${member.user.username}**! To continue, please use the \`/verify\` command.\n\nExample: \`/verify Europe Sabina244\``,
         ),
     ],
   });
