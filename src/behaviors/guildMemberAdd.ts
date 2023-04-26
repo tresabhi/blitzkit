4fe4d93fb00b0eb9dc9099505394a448389ae668
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-26 23:43:13 +0000

    fix: only react to people joining skilled clan

--- src/behaviors/guildMemberAdd.ts
+++ src/behaviors/guildMemberAdd.ts
@@ -3,19 +3,21 @@ import discord from '../../discord.json' assert { type: 'json' };
 import { SKILLED_COLOR } from '../constants/colors.js';
 
 export default async function (member: GuildMember) {
-  console.log(`${member.user.tag} joined`);
+  if (member.guild.id === discord.guild_id) {
+    await (
+      member.guild.channels.cache.get(discord.verify_channel) as TextChannel
+    ).send({
+      content: member.toString(),
+      embeds: [
+        new EmbedBuilder()
+          .setColor(SKILLED_COLOR)
+          .setTitle(`Welcome ${member.user.username}`)
+          .setDescription(
+            `Welcome to the Skilled server, **${member.user.username}**! To continue, please use the \`/verify\` command.\n\nExample: \`/verify Europe Sabina244\``,
+          ),
+      ],
+    });
 
-  await (
-    member.guild.channels.cache.get(discord.verify_channel) as TextChannel
-  ).send({
-    content: member.toString(),
-    embeds: [
-      new EmbedBuilder()
-        .setColor(SKILLED_COLOR)
-        .setTitle(`Welcome ${member.user.username}`)
-        .setDescription(
-          `Welcome to the Skilled server, **${member.user.username}**! To continue, please use the \`/verify\` command.\n\nExample: \`/verify Europe Sabina244\``,
-        ),
-    ],
-  });
+    console.log(`${member.user.tag} joined`);
+  }
 }
