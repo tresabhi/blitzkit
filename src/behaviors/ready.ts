a3b2a34d7fe3e8381daf11a1b28f88fc840ed414
tresabhi <coolabhigyaandeep07@gmail.com>
2023-04-28 13:47:32 +0000

    feat: online message

--- src/behaviors/ready.ts
+++ src/behaviors/ready.ts
@@ -1,7 +1,22 @@
-import { Client } from 'discord.js';
+import { Client, EmbedBuilder, TextChannel } from 'discord.js';
+import discord from '../../discord.json' assert { type: 'json' };
+import { POSITIVE_COLOR } from '../constants/colors.js';
 import isDev from '../utilities/isDev.js';
 
 export default function ready(client: Client<true>) {
+  (
+    client.guilds.cache
+      .get(discord.guild_id)
+      ?.channels.cache.get(discord.log_channel) as TextChannel
+  ).send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle(`${client.user.username} came online`)
+        .setDescription('All systems operating properly')
+        .setColor(POSITIVE_COLOR),
+    ],
+  });
+
   console.log(
     `Logged in as ${client.user.tag} in ${
       isDev() ? 'development' : 'production'
