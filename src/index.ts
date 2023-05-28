4800cadd51a34d0fff682090cdc52a8fc4e6ac5d
tresabhi <coolabhigyaandeep07@gmail.com>
2023-05-28 02:32:49 +0000

    feat: upgrade to node 18

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { registerErrorHandlers } from './events/error.js';
 import guildMemberAdd from './events/guildMemberAdd.js';
 import interactionCreate from './events/interactionCreate.js';
 import ready from './events/ready.js';
+import './fetch-polyfill';
 
 export const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
