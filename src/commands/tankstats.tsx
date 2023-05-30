38fe4bf5da96d06c0d8589ce1d5e475c8ee09a49
tresabhi <coolabhigyaandeep07@gmail.com>
2023-05-30 17:47:40 +0000

    fix: info color as hex

--- src/commands/tankstats.tsx
+++ src/commands/tankstats.tsx
@@ -38,7 +38,7 @@ import resolveTankName from '../utilities/resolveTankName.js';
 
 export default {
   inProduction: true,
-  inDevelopment: true,
+  inDevelopment: false,
   inPublic: true,
 
   command: new SlashCommandBuilder()
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,3 @@
-import { theme } from '../stitches.config.js';
-
-export const ACCENT_COLOR = theme.colors.textHighContrast;
+export const ACCENT_COLOR = '#ecedee'; // theme.colors.textHighContrast
 export const POSITIVE_COLOR = '#82ff29';
 export const NEGATIVE_COLOR = '#ff4747';
