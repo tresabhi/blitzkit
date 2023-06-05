632881c6b082b68313224c2688a7280e96afea12
tresabhi <coolabhigyaandeep07@gmail.com>
2023-06-05 18:47:37 +0000

    feat: removed `node-worker-threads-pool` as satori seems to be faster without it

--- src/core/blitz/tankopedia.ts
+++ src/core/blitz/tankopedia.ts
@@ -1,4 +1,3 @@
-import { StaticPool } from 'node-worker-threads-pool';
 import { WargamingResponse } from '../../types/wargamingResponse.js';
 
 export type TankType = 'AT-SPG' | 'lightTank' | 'mediumTank' | 'heavyTank';
@@ -50,39 +49,3 @@ export const TIER_ROMAN_NUMERALS: Record<number, string> = {
 export const TANK_IDS = Object.keys(tankopedia).map((id) => Number(id));
 export const TANKS = TANK_IDS.map((id) => tankopedia[id]);
 export const TANK_NAMES = TANKS.map((tank) => tank.name);
-
-const imageEncodingStaticPool = new StaticPool({
-  size: 1,
-  task: async (data: Tankopedia) => {
-    console.log('Encoding tank images...');
-
-    const encoded: [number, string][] = [];
-
-    await Promise.all(
-      Object.entries(data as Tankopedia).map(
-        async ([, tank]: [string, TankopediaEntry]) => {
-          if (tank.images.normal) {
-            const response = await fetch(tank.images.normal);
-            const blob = await response.arrayBuffer();
-            const base64 = `data:${response.headers.get(
-              'content-type',
-            )};base64,${Buffer.from(blob).toString('base64')}`;
-            encoded.push([tank.tank_id, base64]);
-          }
-        },
-      ),
-    );
-
-    console.log('Encoded tank images');
-
-    return encoded;
-  },
-});
-
-imageEncodingStaticPool.exec(tankopedia).then((encoded) => {
-  console.log('Implementing encoded images...');
-  encoded.forEach(([tankId, image]) => {
-    tankopedia[tankId].images.normal = image;
-  });
-  console.log('Implemented encoded images');
-});
