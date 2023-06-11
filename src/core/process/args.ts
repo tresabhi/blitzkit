3330dce2643c9809471def41a1073f9492dde066
tresabhi <coolabhigyaandeep07@gmail.com>
2023-06-11 18:28:25 +0000

    feat: better `cyclic.sh` script compatibility

--- src/core/process/args.ts
+++ src/core/process/args.ts
@@ -1,28 +1,9 @@
-import { argv } from 'process';
-import errorWithCause from './errorWithCause.js';
+export const discordToken = process.env.DISCORD_TOKEN!;
+export const wargamingApplicationId = process.env.WARGAMING_APPLICATION_ID!;
 
-type ArgumentName = 'discord-token' | 'wargaming-application-id';
-
-const args: Record<ArgumentName, string> = {
-  'discord-token': '',
-  'wargaming-application-id': '',
-};
-
-argv.forEach((arg) => {
-  if (arg.includes('=')) {
-    const [name, value] = arg.split('=');
-    args[name as ArgumentName] = value;
-  }
-});
-
-Object.keys(args).forEach((arg) => {
-  if (args[arg as ArgumentName] === '') {
-    throw errorWithCause(
-      `Missing argument: ${arg}`,
-      'Missing required auth key.',
-    );
-  }
-});
-
-export const discordToken = args['discord-token'];
-export const wargamingApplicationId = args['wargaming-application-id'];
+if (!discordToken) {
+  throw new Error('No Discord token provided');
+}
+if (!wargamingApplicationId) {
+  throw new Error('No Wargaming application id provided}');
+}
---
+++ src/types/global.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+declare module '*.ttf' {
+  const path: string;
+  export default path;
+}
