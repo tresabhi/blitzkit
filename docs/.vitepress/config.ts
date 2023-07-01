3167e311c20d121d3ab1350cfe8006e01f151a2d
tresabhi <coolabhigyaandeep07@gmail.com>
2023-07-01 19:26:47 +0000

    feat: vitepress template

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "blitzkrieg",
+  description: "🎉 All-in-one Discord bot for everything World of Tanks Blitz",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
