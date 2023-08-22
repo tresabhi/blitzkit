cb65e6b0af24030384c09e776cbb5aaf25ae50b7
tresabhi <coolabhigyaandeep07@gmail.com>
2023-08-22 23:46:59 +0000

    fix: corrected order of changelog versions

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,7 @@
-import { DefaultTheme, defineConfig } from 'vitepress';
+import { compareVersions } from 'compare-versions';
+import { readdirSync } from 'fs';
+import { parse } from 'path';
+import { defineConfig } from 'vitepress';
 import { getSidebar } from 'vitepress-plugin-auto-sidebar';
 
 export default defineConfig({
@@ -12,11 +15,21 @@ export default defineConfig({
       { text: 'Examples', link: '/markdown-examples' },
     ],
 
-    sidebar: getSidebar({
-      collapsed: false,
-      contentDirs: ['guide', 'legal', 'changelogs'],
-      contentRoot: '/docs/',
-    }) as DefaultTheme.Sidebar,
+    sidebar: [
+      ...getSidebar({
+        collapsed: false,
+        contentDirs: ['guide', 'legal'],
+        contentRoot: '/docs/',
+      }),
+
+      {
+        text: 'Changelogs',
+        items: readdirSync('./docs/changelogs')
+          .map((file) => parse(file).name)
+          .sort(compareVersions)
+          .map((name) => ({ text: name, link: `/changelogs/${name}.html` })),
+      },
+    ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
