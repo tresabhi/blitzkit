dafd54f8308e095b816c374dc2e81e78af96a66b
tresabhi <coolabhigyaandeep07@gmail.com>
2023-08-22 23:47:35 +0000

    feat: github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/tresabhi/blitzkrieg' },
     ],
   },
 });
