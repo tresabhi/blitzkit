69e1577d18b484a2e60cc75e3d92eab7ed1481a7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2023-10-16 20:44:47 -0500

    fix: prisma copy over

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,36 +1,44 @@
 import { build } from 'esbuild';
-import { readdir } from 'fs/promises';
+import { cp, readdir } from 'fs/promises';
 
-const FILES = [
-  'bot',
-  'index',
+readdir('src/workers').then((workers) => {
+  const files = [
+    'bot',
+    'index',
 
-  (await readdir('src/workers')).map(
-    (file) => `workers/${file.replace('.ts', '')}`,
-  ),
-];
+    ...workers.map((file) => `workers/${file.replace('.ts', '')}`),
+  ];
 
-await build({
-  entryPoints: FILES.map((file) => `src/${file}.ts`),
-  outdir: 'dist/bot',
+  build({
+    entryPoints: files.map((file) => `src/${file}.ts`),
+    outdir: 'dist/bot',
 
-  platform: 'node',
-  tsconfig: 'tsconfig.esbuild.json',
-  loader: {
-    '.node': 'copy',
-    '.ttf': 'file',
-  },
-  format: 'cjs',
-  jsx: 'transform',
-  outExtension: {
-    '.js': '.cjs',
-  },
+    platform: 'node',
+    tsconfig: 'tsconfig.esbuild.json',
+    loader: {
+      '.node': 'copy',
+      '.ttf': 'file',
+    },
+    format: 'cjs',
+    jsx: 'transform',
+    outExtension: {
+      '.js': '.cjs',
+    },
 
-  bundle: true,
-  sourcemap: true,
-  minifyIdentifiers: false, // cause goofy errors
-  minifySyntax: true,
-  minifyWhitespace: true,
+    bundle: true,
+    sourcemap: true,
+    minifyIdentifiers: false, // cause goofy errors
+    minifySyntax: true,
+    minifyWhitespace: true,
 
-  logLevel: 'info',
+    logLevel: 'info',
+  });
 });
+
+// copy schema.prisma
+cp('prisma/schema.prisma', 'dist/bot/schema.prisma');
+readdir('node_modules/prisma').then((files) =>
+  files
+    .filter((file) => file.endsWith('.node'))
+    .map((file) => cp(`node_modules/prisma/${file}`, `dist/bot/${file}`)),
+);
