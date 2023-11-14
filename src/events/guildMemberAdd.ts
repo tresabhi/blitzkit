286936131df9d817daa39ebd44a943308570e1b2
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2023-11-13 21:16:45 -0600

    feat: simpler welcome message

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,12 +1,5 @@
-import {
-  AttachmentBuilder,
-  EmbedBuilder,
-  GuildMember,
-  TextChannel,
-} from 'discord.js';
-import markdownEscape from 'markdown-escape';
+import { AttachmentBuilder, GuildMember, TextChannel } from 'discord.js';
 import discord from '../../discord.json' assert { type: 'json' };
-import { ACCENT_COLOR } from '../constants/colors';
 
 export default async function (member: GuildMember) {
   if (member.guild.id === discord.sklld_guild_id) {
@@ -15,15 +8,7 @@ export default async function (member: GuildMember) {
         discord.sklld_verify_channel,
       ) as TextChannel
     ).send({
-      content: member.toString(),
-      embeds: [
-        new EmbedBuilder()
-          .setColor(ACCENT_COLOR)
-          .setTitle(`Welcome, ${markdownEscape(member.user.displayName)}`)
-          .setDescription(
-            `Welcome to the Skilled server! To continue, please use the \`/link\` command.`,
-          ),
-      ],
+      content: `# Welcome, ${member.toString()}!\nPlease use the \`/link\` command like the video below to continue.`,
       files: [new AttachmentBuilder('https://i.imgur.com/2p6GFgC.gif')],
     });
   }
