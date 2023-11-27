5ca64f839e9d99f3206d67f52e28e0c4351f0132
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2023-11-27 12:12:57 -0600

    fix: exhaustive user and channel indexing

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,8 +5,17 @@ export default function ready(client: Client<true>) {
 
   const activities: (() => string)[] = [
     () => `Living in ${client.guilds.cache.size.toLocaleString()} servers`,
-    () => `Serving ${client.users.cache.size.toLocaleString()} users`,
-    () => `Observing ${client.channels.cache.size.toLocaleString()} channels`,
+    () =>
+      `Serving ${client.guilds.cache
+        .reduce((accumulator, guild) => accumulator + guild.memberCount, 0)
+        .toLocaleString()} users`,
+    () =>
+      `Observing ${client.guilds.cache
+        .reduce(
+          (accumulator, guild) => accumulator + guild.channels.cache.size,
+          0,
+        )
+        .toLocaleString()} channels`,
   ];
 
   client.user.setPresence({
