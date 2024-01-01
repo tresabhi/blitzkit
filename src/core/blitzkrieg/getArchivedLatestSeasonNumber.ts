90ee71c9ca35b03c00ffe992de1461af51d44a7c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-01-01 14:19:13 -0600

    fix: archived season number

--- src/core/blitzkrieg/getArchivedLatestSeasonNumber.ts
+++ src/core/blitzkrieg/getArchivedLatestSeasonNumber.ts
@@ -1,17 +1,28 @@
 import getRatingsInfo from '../blitz/getRatingsInfo';
 
+interface GitHubTrees {
+  tree: { path: string; url: string }[];
+}
+
 let cachedLatestArchivedSeasonNumber: number | null = null;
 
 export async function getArchivedLatestSeasonNumber() {
+  if (cachedLatestArchivedSeasonNumber !== null) {
+    return cachedLatestArchivedSeasonNumber;
+  }
+
   const ratingsInfo = await getRatingsInfo('com');
 
   if (ratingsInfo.detail) {
-    const comURL = (
+    const regionsURL = (
       (await fetch(
-        'https://api.github.com/repos/tresabhi/blitzkrieg-assets/git/trees/main/regions',
-      ).then((response) => response.json())) as {
-        tree: { path: string; url: string }[];
-      }
+        'https://api.github.com/repos/tresabhi/blitzkrieg-assets/git/trees/main',
+      ).then((response) => response.json())) as GitHubTrees
+    ).tree.find(({ path }) => path === 'regions')!.url;
+    const comURL = (
+      (await fetch(regionsURL).then((response) =>
+        response.json(),
+      )) as GitHubTrees
     ).tree.find(({ path }) => path === 'com')!.url;
     const ratingsURL = (
       (await fetch(comURL).then((response) => response.json())) as {
@@ -20,14 +31,16 @@ export async function getArchivedLatestSeasonNumber() {
     ).tree[0].url;
     const latestSeason = Math.max(
       ...(
-        (await fetch(ratingsURL).then((response) => {
-          response.json();
-        })) as { tree: { path: string }[] }
-      ).tree.map(({ path }) => parseInt(path)),
+        (await fetch(ratingsURL).then((response) => response.json())) as {
+          tree: { path: string }[];
+        }
+      ).tree.map(({ path }) => Number(path)),
     );
 
-    return latestSeason;
+    cachedLatestArchivedSeasonNumber = latestSeason;
   } else {
-    return ratingsInfo.current_season - 1;
+    cachedLatestArchivedSeasonNumber = ratingsInfo.current_season - 1;
   }
+
+  return cachedLatestArchivedSeasonNumber;
 }
