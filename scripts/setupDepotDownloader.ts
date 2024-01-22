d70a9800583afa359f63cf4ffa4ecf3a8526cb89
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-01-22 13:40:37 -0600

    feat: depot dodnloader node js

---
+++ scripts/setupDepotDownloader.ts
@@ -0,0 +1,27 @@
+import decompress from 'decompress';
+import { existsSync } from 'fs';
+import { mkdir, writeFile } from 'fs/promises';
+
+if (!existsSync('temp')) {
+  console.log('Creating temp directory...');
+  await mkdir('temp');
+}
+
+console.log('Downloading depot downloader...');
+await fetch(
+  'https://api.github.com/repos/SteamRE/DepotDownloader/releases/latest',
+)
+  .then(
+    (response) =>
+      response.json() as Promise<{
+        assets: { browser_download_url: string }[];
+      }>,
+  )
+  .then(({ assets }) => fetch(assets[0].browser_download_url))
+  .then((response) => response.arrayBuffer())
+  .then((arrayBuffer) =>
+    writeFile('temp/depotDownloader.zip', Buffer.from(arrayBuffer)),
+  );
+
+console.log('Unzipping...');
+decompress('temp/depotDownloader.zip', 'temp/depotDownloader');
