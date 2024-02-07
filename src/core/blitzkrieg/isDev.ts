92bb4af5ecc6abf99c1c3d9f2c0827c037210ac2
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-02-07 13:40:14 -0600

    fix: test

--- src/core/blitzkrieg/isDev.ts
+++ src/core/blitzkrieg/isDev.ts
@@ -1,6 +1,11 @@
 const devHostStartNames = ['blitzkrieg-', 'localhost'];
 
 export default function isDev() {
+  console.log(
+    process.env,
+    typeof window !== 'undefined' && window.location.host,
+  );
+
   return (
     process.env.NODE_ENV === 'development' ||
     (typeof window !== 'undefined' &&
