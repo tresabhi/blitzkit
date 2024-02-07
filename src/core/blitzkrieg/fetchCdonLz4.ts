846c2a496256afcdf1697152f2406c0dc5dd2b0e
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-02-07 13:32:23 -0600

    fix: something

--- src/core/blitzkrieg/fetchCdonLz4.ts
+++ src/core/blitzkrieg/fetchCdonLz4.ts
@@ -1,9 +1,7 @@
 import { superDecompress } from './superDecompress';
 
 export async function fetchCdonLz4<Type>(url: string) {
-  return await fetch(url, {
-    cache: 'no-cache',
-  }).then(async (response) => {
-    return superDecompress(Buffer.from(await response.arrayBuffer())) as Type;
-  });
+  return await fetch(url, { cache: 'no-cache' })
+    .then((response) => response.arrayBuffer())
+    .then((buffer) => superDecompress<Type>(new Uint8Array(buffer)));
 }
