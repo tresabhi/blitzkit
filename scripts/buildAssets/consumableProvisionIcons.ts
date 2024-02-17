34ba3ffdde195b192b7a71fec2fdea5da1a63cd7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-02-17 10:36:00 -0600

    feat: better cropped icons

--- scripts/buildAssets/consumableProvisionIcons.ts
+++ scripts/buildAssets/consumableProvisionIcons.ts
@@ -66,7 +66,9 @@ export async function consumableProvisionIcons(production: boolean) {
         const image = sharp(
           await readDVPLFile(`${DATA}/${configPath}.packed.webp.dvpl`),
         );
-        const content = (await image.trim().toBuffer()).toString('base64');
+        const content = (
+          await image.trim({ threshold: 100 }).toBuffer()
+        ).toString('base64');
 
         changes.push({
           path: `icons/consumables/${consumable.id}.webp`,
@@ -123,7 +125,9 @@ export async function consumableProvisionIcons(production: boolean) {
         const image = sharp(
           await readDVPLFile(`${DATA}/${configPath}.packed.webp.dvpl`),
         );
-        const content = (await image.trim().toBuffer()).toString('base64');
+        const content = (
+          await image.trim({ threshold: 100 }).toBuffer()
+        ).toString('base64');
 
         changes.push({
           path: `icons/provisions/${provision.id}.webp`,
