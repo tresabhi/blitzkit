2610697d86bbc60a45b3e881715b64dcbfea864b
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-03-09 23:04:09 -0600

    wee ooo wee oo

--- src/core/blitzkrieg/jsxToSvg/index.ts
+++ src/core/blitzkrieg/jsxToSvg/index.ts
@@ -3,17 +3,17 @@ import satori from 'satori';
 
 const fonts = [
   {
-    data: readFileSync(require('./Roboto.ttf')),
+    file: require('./Roboto.ttf') as string,
     weight: 400 as const,
     name: 'Roboto',
   },
   {
-    data: readFileSync(require('./Roboto-Bold.ttf')),
+    file: require('./Roboto-Bold.ttf') as string,
     weight: 700 as const,
     name: 'Roboto-Bold',
   },
   {
-    data: readFileSync(require('./Roboto-Black.ttf')),
+    file: require('./Roboto-Black.ttf') as string,
     weight: 900 as const,
     name: 'Roboto-Black',
   },
@@ -22,6 +22,9 @@ const fonts = [
 export default async function jsxToSvg(jsx: JSX.Element) {
   return await satori(jsx, {
     width: 480,
-    fonts,
+    fonts: fonts.map(({ file, ...font }) => ({
+      data: readFileSync(`${__dirname}/${file}`),
+      ...font,
+    })),
   });
 }
