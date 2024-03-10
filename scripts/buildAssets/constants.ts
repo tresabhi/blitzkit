f91bd63646337b78156b049f0c1c840de340a076
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-03-10 12:50:25 -0500

    whooped

--- scripts/buildAssets/constants.ts
+++ scripts/buildAssets/constants.ts
@@ -1,12 +1,13 @@
+import { readdir } from 'fs/promises';
 import { argv } from 'process';
 
 const isDepot = argv.includes('--depot');
 export const WOTB_WIN32_DEPOT = 444202;
 export const DATA = isDepot
-   ? await (async () => {
-         const [installationVersion] = await readdir(`depots/${WOTB_WIN32_DEPOT}`);
-         return `depots/${WOTB_WIN32_DEPOT}/${installationVersion}/Data`;
-       })()
+  ? await (async () => {
+      const [installationVersion] = await readdir(`depots/${WOTB_WIN32_DEPOT}`);
+      return `depots/${WOTB_WIN32_DEPOT}/${installationVersion}/Data`;
+    })()
   : 'C:/Program Files (x86)/Steam/steamapps/common/World of Tanks Blitz/Data';
 
 export const POI = {
