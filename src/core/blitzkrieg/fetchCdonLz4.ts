d9a34dc60e2b618459dda3a7a295da34568d3f4e
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-03-13 17:52:06 -0500

    fix: use no-store

--- src/core/blitzkrieg/fetchCdonLz4.ts
+++ src/core/blitzkrieg/fetchCdonLz4.ts
@@ -1,7 +1,7 @@
 import { superDecompress } from './superDecompress';
 
 export async function fetchCdonLz4<Type>(url: string) {
-  return await fetch(url, { cache: 'no-cache' })
+  return await fetch(url, { cache: 'no-store' })
     .then((response) => response.arrayBuffer())
     .then((buffer) => superDecompress<Type>(new Uint8Array(buffer)));
 }
