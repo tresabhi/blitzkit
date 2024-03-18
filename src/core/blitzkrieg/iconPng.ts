bd3f5dea0575bceb3b9787f229ba31cb423ab85c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-03-17 21:23:57 -0500

    feat: optimized png cache

--- src/core/blitzkrieg/iconPng.ts
+++ src/core/blitzkrieg/iconPng.ts
@@ -1,14 +1,14 @@
 import sharp from 'sharp';
 
-const cache: Record<string, string> = {};
+const cache: Record<string, Buffer> = {};
 
 export async function iconPng(url: string) {
-  if (cache[url]) return cache[url];
+  if (!cache[url]) {
+    const imageResponse = await fetch(url);
+    const imageBufferWEBP = await imageResponse.arrayBuffer();
+    const imageBufferPNG = await sharp(imageBufferWEBP).png().toBuffer();
+    cache[url] = imageBufferPNG;
+  }
 
-  const imageResponse = await fetch(url);
-  const imageBufferWEBP = await imageResponse.arrayBuffer();
-  const imageBufferPNG = await sharp(imageBufferWEBP).png().toBuffer();
-  cache[url] = `data:image/png;base64,${imageBufferPNG.toString('base64')}`;
-
-  return cache[url];
+  return `data:image/png;base64,${cache[url].toString('base64')}`;
 }
