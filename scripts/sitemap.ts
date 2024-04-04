c96655a6c0777af7eb8d591007ca5304c7ee12b1
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-03 22:47:10 -0500

    feat: xml

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -1,13 +1,26 @@
 import { tankDefinitions } from '../src/core/blitzkrieg/tankDefinitions';
 
+// random vercel bug forces require import
+const { writeFileSync } = require('fs');
+
 console.log('Building sitemap...');
 
 const awaitedTankDefinitions = await tankDefinitions;
 const values = Object.values(awaitedTankDefinitions);
-const sitemap = values
+const txt = values
   .map((tank) => `https://blitz-krieg.vercel.app/tools/tankopedia/${tank.id}/`)
   .join('\n');
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+${values
+  .map(
+    (tank) =>
+      `<url><loc>https://blitz-krieg.vercel.app/tools/tankopedia/${tank.id}/</loc></url>`,
+  )
+  .join('\n')}
+</urlset>`;
 
-require('fs').writeFileSync('public/sitemap.txt', sitemap);
+writeFileSync('public/sitemap.txt', txt);
+writeFileSync('public/sitemap.xml', xml);
 
 console.log(`Built sitemap for ${values.length} tanks`);
