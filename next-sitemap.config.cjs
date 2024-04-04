3322fa97d49d38061dd4a5f42007b666e2cde565
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-03 22:14:55 -0500

    feat: ts next config

--- next-sitemap.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://blitz-krieg.vercel.app',
-  generateRobotsTxt: true,
-  sourceDir: 'dist/website',
-};
