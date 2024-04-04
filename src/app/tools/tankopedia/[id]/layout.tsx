70f769846612bfdc49d236bd98345fc767777342
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-03 19:48:28 -0500

    feat: preview image

---
+++ src/app/tools/tankopedia/[id]/layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+import { tankDefinitions } from '../../../../core/blitzkrieg/tankDefinitions';
+import { tankIcon } from '../../../../core/blitzkrieg/tankIcon';
+
+interface TankopediaLayoutProps {
+  children: ReactNode;
+  params: { id: string };
+}
+
+export default async function TankopediaLayout({
+  children,
+  params,
+}: TankopediaLayoutProps) {
+  const id = Number(params.id);
+  const awaitedTankDefinitions = await tankDefinitions;
+  const tank = awaitedTankDefinitions[id];
+
+  return (
+    <>
+      <title>{tank.name}</title>
+      <meta
+        name="description"
+        content={`Statistics, armor profiles, equipment, and more about ${tank.name}`}
+      />
+      <meta property="og:image" content={tankIcon(id)} />
+
+      {children}
+    </>
+  );
+}
