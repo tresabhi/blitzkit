a9dc9e7ebda3f528d31edace03346cf3f365403e
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-04 10:16:47 -0500

    feat: cleaner tool shadows

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -3,7 +3,7 @@ import { theme } from '../stitches.config';
 
 export const tool = styleVariants({
   enabled: {
-    boxShadow: `inset 0 -256px 512px -128px ${theme.colors.appBackground1}`,
+    boxShadow: `inset 0 -200px 128px -128px ${theme.colors.appBackground1}`,
 
     ':hover': {
       boxShadow: `inset 0 -160px 128px -128px ${theme.colors.appBackground1}`,
