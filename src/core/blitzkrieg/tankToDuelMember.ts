e7e74f706e73bc8d6226d9fcecf988749352b881
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-09 10:51:12 -0500

    feat: more provisions

--- src/core/blitzkrieg/tankToDuelMember.ts
+++ src/core/blitzkrieg/tankToDuelMember.ts
@@ -4,6 +4,12 @@ import { availableProvisions } from './availableProvisions';
 import { ProvisionDefinitions } from './provisionDefinitions';
 import { TankDefinition } from './tankDefinitions';
 
+const PROVISION_PREFERENCES = [
+  19, // improved fuel
+  18, // standard fuel
+  22, // protective kit
+];
+
 export function tankToDuelMember(
   tank: TankDefinition,
   provisionDefinitions: ProvisionDefinitions,
@@ -19,8 +25,14 @@ export function tankToDuelMember(
     cooldownBooster: 0,
     crewMastery: 1,
     provisions: provisionsList
-      .filter((provision) => provision.crew !== undefined)
-      .map((provision) => provision.id),
+      .sort(
+        (a, b) =>
+          PROVISION_PREFERENCES.indexOf(a.id) -
+          PROVISION_PREFERENCES.indexOf(b.id),
+      )
+      .sort((a, b) => (b.crew ?? 0) - (a.crew ?? 0))
+      .slice(0, tank.provisions)
+      .map(({ id }) => id),
     engine: tank.engines.at(-1)!,
     gun,
     shell: tank.turrets.at(-1)!.guns.at(-1)!.shells[0],
