e834d31d92fe1ba46e48f683c6ea917abfc9ca02
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-18 18:23:23 -0500

    feat: use lz4js types instead of home brewed

--- src/types/lz4js.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'lz4js' {
-  export function compress(buffer: Uint8Array, maxSize?: number): Uint8Array;
-  export function decompress(buffer: Uint8Array, maxSize?: number): Uint8Array;
-}
