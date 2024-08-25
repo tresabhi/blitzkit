cae2bcc3ea106c0c9d260b63f176e4d82bc40cde
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-08-24 20:23:58 -0500

    fix: it didn't have to end this way

--- src/constants/youtubers.ts
+++ src/constants/youtubers.ts
@@ -1,5 +1,4 @@
 export const youtubers = [
-  { id: 'UCKBYXp4Xn2I2tL1UL4fpbhw', name: 'Droodles' },
   { id: 'UCrQ-Dy8lVsm11u8pCJ8W7Tw', name: 'HisRoyalFatness' },
   { id: 'UCxRpZqMElssTdPYDK3x7wyg', name: 'N90' },
 ];
