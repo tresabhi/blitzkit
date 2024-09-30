9b134fab27e55101f395602705b23e8e318389cf
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-09-29 22:09:43 -0500

    move

--- packages/scripts/src/protos.ts
+++ packages/scripts/src/protos.ts
@@ -1,6 +1,6 @@
 import { exec as execSync } from 'child_process';
 import { existsSync } from 'fs';
-import { cp, mkdir, readdir, rm, writeFile } from 'fs/promises';
+import { mkdir, readdir, rm } from 'fs/promises';
 import { promisify } from 'util';
 
 const ROOT = 'packages/core/src/protos';
@@ -19,23 +19,25 @@ const files = await readdir(`${ROOT}/raw`);
 const args = [
   `--plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts${executableFileExtension}"`,
   '--ts_opt=esModuleInterop=true',
-  `--js_out="import_style=commonjs,binary:./${ROOT}/generated"`,
-  `--ts_out="./${ROOT}/generated"`,
+  `--js_out="import_style=commonjs,binary:."`,
+  `--ts_out="."`,
   ...files.map((file) => `${ROOT}/raw/${file}`),
 ];
 
 await rm(`${ROOT}/generated`, { recursive: true, force: true });
+await rm(`temp/protos`, { recursive: true, force: true });
 await mkdir(`${ROOT}/generated`, { recursive: true });
+await mkdir(`temp/protos`, { recursive: true });
 await exec(`protoc ${args.join(' ')}`);
-await cp(`${ROOT}/generated/${ROOT}/raw`, `${ROOT}/generated`, {
-  recursive: true,
-});
-await rm(`${ROOT}/generated/packages`, { recursive: true, force: true });
+// await cp(`${ROOT}/generated/${ROOT}/raw`, `${ROOT}/generated`, {
+//   recursive: true,
+// });
+// await rm(`${ROOT}/generated/packages`, { recursive: true, force: true });
 
-await writeFile(
-  `${ROOT}/generated/index.ts`,
-  files
-    .map((file) => file.replace('.proto', ''))
-    .map((file) => `export * from './${file}_pb';`)
-    .join('\n'),
-);
+// await writeFile(
+//   `${ROOT}/generated/index.ts`,
+//   files
+//     .map((file) => file.replace('.proto', ''))
+//     .map((file) => `export * from './${file}_pb';`)
+//     .join('\n'),
+// );
