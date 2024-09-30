57170469f2daab7b1f699e3855a24ca1b4cf2952
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-09-29 22:24:35 -0500

    feat: some work on functional pro ts

--- packages/scripts/src/buildAssets/definitions.ts
+++ packages/scripts/src/buildAssets/definitions.ts
@@ -1,3 +1,18 @@
+import { TankDefinition, TankDefinitions } from '@blitzkit/core';
+
 export async function definitions() {
   console.log('Building definitions...');
+
+  const tankDefinitions = new TankDefinitions();
+  const tanks = tankDefinitions.getTanksMap();
+
+  const testTank = new TankDefinition();
+
+  testTank.setName('asd');
+
+  tanks.set(1, testTank);
+
+  console.log(JSON.stringify(tankDefinitions.toObject(), null, 2));
 }
+
+definitions();
--- packages/scripts/src/protos.ts
+++ packages/scripts/src/protos.ts
@@ -1,6 +1,6 @@
 import { exec as execSync } from 'child_process';
 import { existsSync } from 'fs';
-import { mkdir, readdir, rm } from 'fs/promises';
+import { readdir, rm, writeFile } from 'fs/promises';
 import { promisify } from 'util';
 
 const ROOT = 'packages/core/src/protos';
@@ -15,29 +15,29 @@ if (existsSync('node_modules/.bin/protoc-gen-ts.exe')) {
   executableFileExtension = '';
 }
 
-const files = await readdir(`${ROOT}/raw`);
+const filesRaw = await readdir(`${ROOT}`);
+await Promise.all(
+  filesRaw.map(async (file) => {
+    if (!file.endsWith('.proto')) {
+      await rm(`${ROOT}/${file}`);
+    }
+  }),
+);
+
+const files = filesRaw.filter((file) => file.endsWith('.proto'));
 const args = [
   `--plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts${executableFileExtension}"`,
   '--ts_opt=esModuleInterop=true',
   `--js_out="import_style=commonjs,binary:."`,
   `--ts_out="."`,
-  ...files.map((file) => `${ROOT}/raw/${file}`),
+  ...files.map((file) => `${ROOT}/${file}`),
 ];
 
-await rm(`${ROOT}/generated`, { recursive: true, force: true });
-await rm(`temp/protos`, { recursive: true, force: true });
-await mkdir(`${ROOT}/generated`, { recursive: true });
-await mkdir(`temp/protos`, { recursive: true });
 await exec(`protoc ${args.join(' ')}`);
-// await cp(`${ROOT}/generated/${ROOT}/raw`, `${ROOT}/generated`, {
-//   recursive: true,
-// });
-// await rm(`${ROOT}/generated/packages`, { recursive: true, force: true });
-
-// await writeFile(
-//   `${ROOT}/generated/index.ts`,
-//   files
-//     .map((file) => file.replace('.proto', ''))
-//     .map((file) => `export * from './${file}_pb';`)
-//     .join('\n'),
-// );
+await writeFile(
+  `${ROOT}/index.ts`,
+  files
+    .map((file) => file.replace('.proto', ''))
+    .map((file) => `export * from './${file}_pb';`)
+    .join('\n'),
+);
