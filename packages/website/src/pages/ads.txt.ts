7af06d21204628f8c1d9957b116372f23bdaa25e
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-11 10:49:29 -0500

    fix: assert ads.txt

--- packages/website/src/pages/ads.txt.ts
+++ packages/website/src/pages/ads.txt.ts
@@ -1,10 +1,13 @@
+import { assertSecret } from '@blitzkit/core';
 import type { APIContext } from 'astro';
 
 export const prerender = false;
 
 export async function GET({}: APIContext) {
   const response = await fetch(
-    `https://api.nitropay.com/v1/ads-${import.meta.env.PUBLIC_NITROPAY_ID}.txt`,
+    `https://api.nitropay.com/v1/ads-${assertSecret(
+      import.meta.env.PUBLIC_NITROPAY_ID,
+    )}.txt`,
   );
   const text = await response.text();
   const date = new Date();
