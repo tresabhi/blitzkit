b52f742527610477e939845f78788d3ae4a9a8bf
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-18 10:50:03 -0500

    feat: remove vercel

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,14 +2,12 @@
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   devToolbar: { enabled: false },
-  output: 'hybrid',
-  adapter: vercel(),
+  output: 'static',
   site: 'https://blitzkit.app',
 
   integrations: [react(), sitemap()],
