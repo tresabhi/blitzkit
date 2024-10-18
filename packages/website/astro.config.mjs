4e6a1de6ca6689111347d4ab8ad4ecb724a233c2
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-18 10:52:23 -0500

    fix: add outdir

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   devToolbar: { enabled: false },
   output: 'static',
   site: 'https://blitzkit.app',
+  outDir: '../../dist/website',
 
   integrations: [react(), sitemap()],
   vite: {
