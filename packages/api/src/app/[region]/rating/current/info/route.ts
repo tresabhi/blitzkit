da8025a0ad193b6e57a631687cf025f9197fa596
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-18 10:24:52 -0500

    feat: remove api

--- packages/api/src/app/[region]/rating/current/info/route.ts
+++
@@ -1,9 +0,0 @@
-import { getRatingInfo, Region } from '@blitzkit/core';
-import { NextResponse } from 'next/server';
-
-export async function GET(
-  request: Request,
-  { params }: { params: { region: Region } },
-) {
-  return NextResponse.json(await getRatingInfo(params.region));
-}
