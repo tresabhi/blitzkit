236271d2795345c727971a5ddf3470ba37dbd5fe
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-20 01:01:54 -0500

    feat: cooler header

--- packages/website/src/components/Plugs.tsx
+++ packages/website/src/components/Plugs.tsx
@@ -12,6 +12,13 @@ export function Plugs() {
 }
 
 function Content() {
+  const promoteOpentest =
+    assertSecret(import.meta.env.PUBLIC_PROMOTE_OPENTEST) === 'true' &&
+    assertSecret(import.meta.env.PUBLIC_ASSET_BRANCH) !== 'opentest';
+  const promotions = [promoteOpentest];
+
+  if (promotions.every((promotion) => !promotion)) return null;
+
   return (
     <Flex
       align="center"
@@ -21,24 +28,23 @@ function Content() {
       py="8"
       gap="6"
     >
-      {assertSecret(import.meta.env.PUBLIC_PROMOTE_OPENTEST) === 'true' &&
-        assertSecret(import.meta.env.PUBLIC_ASSET_BRANCH) !== 'opentest' && (
-          <Flex direction="column" gap="3" align="center">
-            <Text size="5">
-              BlitzKit{' '}
-              <Code variant="outline" size="4" color="gray" highContrast>
-                opentest
-              </Code>{' '}
-              is available now{' '}
-            </Text>
+      {promoteOpentest && (
+        <Flex direction="column" gap="3" align="center">
+          <Text size="5">
+            BlitzKit{' '}
+            <Code variant="outline" size="4" color="gray" highContrast>
+              opentest
+            </Code>{' '}
+            is available now{' '}
+          </Text>
 
-            <Link href="https://opentest.blitzkit.app/" target="_blank">
-              <Button color="green">
-                BlitzKit opentest <ArrowRightIcon />
-              </Button>
-            </Link>
-          </Flex>
-        )}
+          <Link href="https://opentest.blitzkit.app/" target="_blank">
+            <Button color="green">
+              BlitzKit opentest <ArrowRightIcon />
+            </Button>
+          </Link>
+        </Flex>
+      )}
     </Flex>
   );
 }
