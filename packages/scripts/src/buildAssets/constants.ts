d8f6e8fe8e23231fd2b9a19635b01d331902916d
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-21 11:31:17 -0500

    fix: patches

--- packages/scripts/src/buildAssets/constants.ts
+++ packages/scripts/src/buildAssets/constants.ts
@@ -5,7 +5,7 @@ const isDepot = argv.includes('--depot');
 
 export const DATA = isDepot
   ? await (async () => {
-      const [installationVersion] = await readdir(`depots/444202`);
-      return `depots/444202/${installationVersion}/Data`;
+      const [installationVersion] = await readdir(`../../depots/444202`);
+      return `../../depots/444202/${installationVersion}/Data`;
     })()
   : 'C:/Program Files (x86)/Steam/steamapps/common/World of Tanks Blitz/Data';
