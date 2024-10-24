05c8e50e8249b569c47874847ab23fcf17c1a44c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-24 17:53:47 -0500

    fix: remove searching

--- packages/website/src/stores/galleryEphemeral.ts
+++ packages/website/src/stores/galleryEphemeral.ts
@@ -3,14 +3,11 @@ import { subscribeWithSelector } from 'zustand/middleware';
 import { createContextualStore } from '../core/zustand/createContextualStore';
 
 interface GalleryEphemeral {
-  searching: boolean;
   search?: string;
 }
 
 export const GalleryEphemeral = createContextualStore(() => {
   return create<GalleryEphemeral>()(
-    subscribeWithSelector<GalleryEphemeral>(() => ({
-      searching: false,
-    })),
+    subscribeWithSelector<GalleryEphemeral>(() => ({})),
   );
 });
