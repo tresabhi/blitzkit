f1c50c2531859198ac33a9510e09c3d1bd1bcf14
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-10-24 15:37:14 -0500

    fix: consistent lighting in static mode

--- packages/website/src/components/Tankopedia/HeroSection/components/TankSandbox/components/Lighting.tsx
+++ packages/website/src/components/Tankopedia/HeroSection/components/TankSandbox/components/Lighting.tsx
@@ -2,10 +2,14 @@ import { Environment } from '@react-three/drei';
 import { useModel } from '../../../../../../hooks/useModel';
 import { Duel } from '../../../../../../stores/duel';
 import { TankopediaPersistent } from '../../../../../../stores/tankopediaPersistent';
+import { TankopediaDisplay } from '../../../../../../stores/tankopediaPersistent/constants';
 
 export function Lighting() {
   const protagonist = Duel.use((state) => state.protagonist);
+  const display = TankopediaPersistent.use((state) => state.display);
   const { hasPbr } = useModel(protagonist.tank.id);
+  const isBrighterLighting =
+    hasPbr && display !== TankopediaDisplay.StaticArmor;
   const showEnvironment = TankopediaPersistent.use(
     (state) => state.showEnvironment,
   );
@@ -17,13 +21,13 @@ export function Lighting() {
 
       <directionalLight
         position={[1, 1, -1]}
-        intensity={hasPbr ? 1 : 5}
+        intensity={isBrighterLighting ? 5 : 1}
         castShadow
         color={'rgb(225, 225, 255)'}
       />
       <directionalLight
         position={[-1, 1, 1]}
-        intensity={hasPbr ? 1 : 4}
+        intensity={isBrighterLighting ? 4 : 1}
         castShadow
         color={'rgb(240, 255, 240)'}
       />
