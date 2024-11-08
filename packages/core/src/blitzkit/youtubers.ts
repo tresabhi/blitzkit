ea9cb79d84e9a19e108bdd28512e9aeb0a03de23
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-11-08 17:02:45 -0600

    feat: new youtubers

--- packages/core/src/blitzkit/youtubers.ts
+++ packages/core/src/blitzkit/youtubers.ts
@@ -2,4 +2,6 @@ export const youtubers = [
   { id: 'UC9lE09jxmMgMoG13WrNHPmA', name: 'World of Tanks Blitz' },
   { id: 'UCrQ-Dy8lVsm11u8pCJ8W7Tw', name: 'HisRoyalFatness' },
   { id: 'UCxRpZqMElssTdPYDK3x7wyg', name: 'N90' },
+  { id: 'UC28Hc6p2MGoB5s78eHCYkQg', name: 'GuidesBlitz' },
+  { id: 'UCLMEfqtlwLDN-UXX21RiEww', name: 'WoT Blitz Best Replays' },
 ];
