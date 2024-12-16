4f8c1d416f59b6dd8965f9b2e640641a2e67c39b
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-16 10:50:52 -0600

    feat: delete rando

--- packages/bot/src/core/localization/strings/constants.ts
+++ packages/bot/src/core/localization/strings/constants.ts
@@ -3,8 +3,8 @@ import { Locale } from 'discord.js';
 export const SUPPORTED_LOCALES: Locale[] = [
   Locale.EnglishUS,
   Locale.SpanishES,
-  Locale.ChineseCN,
   Locale.Croatian,
   Locale.Russian,
   Locale.Ukrainian,
+  Locale.ChineseCN,
 ];
