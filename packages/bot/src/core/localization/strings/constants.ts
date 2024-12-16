6d448dc5c494a5c14b5f60bfca3e5fd81b9ad79c
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-16 10:49:18 -0600

    feat: new languages

--- packages/bot/src/core/localization/strings/constants.ts
+++ packages/bot/src/core/localization/strings/constants.ts
@@ -1,3 +1,10 @@
 import { Locale } from 'discord.js';
 
-export const SUPPORTED_LOCALES: Locale[] = [Locale.EnglishUS, Locale.SpanishES];
+export const SUPPORTED_LOCALES: Locale[] = [
+  Locale.EnglishUS,
+  Locale.SpanishES,
+  Locale.ChineseCN,
+  Locale.Croatian,
+  Locale.Russian,
+  Locale.Ukrainian,
+];
