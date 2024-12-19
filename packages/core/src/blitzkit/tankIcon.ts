e4f0120d67348ebbf3cf592f1d0860f7d44e3cc4
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-19 10:19:26 -0600

    feat: use blitzkit icons by default

--- packages/core/src/blitzkit/tankIcon.ts
+++ packages/core/src/blitzkit/tankIcon.ts
@@ -2,6 +2,9 @@ import { asset } from '@blitzkit/core';
 
 export const BLITZKIT_TANK_ICON_SIZE = { width: 40 * 16, height: 30 * 16 };
 
-export function tankIcon(id: number, size: 'big' | 'small' = 'big') {
+export function tankIcon(
+  id: number,
+  size: 'big' | 'small' | 'blitzkit' = 'blitzkit',
+) {
   return asset(`icons/tanks/${size}/${id}.webp`);
 }
