9be6725043a64ba49801bbfb0b074cd43138c583
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-27 14:03:43 -0600

    feat: some more localized links

--- packages/website/src/pages/[...locale]/tools/tankopedia/_index.tsx
+++ packages/website/src/pages/[...locale]/tools/tankopedia/_index.tsx
@@ -1,16 +1,23 @@
 import { PageWrapper } from '../../../../components/PageWrapper';
 import { TankSearch } from '../../../../components/TankSearch';
+import { LocaleProvider } from '../../../../hooks/useLocale';
 import { App } from '../../../../stores/app';
 import { TankopediaPersistent } from '../../../../stores/tankopediaPersistent';
 
-export function Page() {
+interface PageProps {
+  locale: string | undefined;
+}
+
+export function Page({ locale }: PageProps) {
   return (
-    <PageWrapper color="purple" maxWidth="80rem">
-      <TankopediaPersistent.Provider>
-        <App.Provider>
-          <TankSearch />
-        </App.Provider>
-      </TankopediaPersistent.Provider>
-    </PageWrapper>
+    <LocaleProvider locale={locale}>
+      <PageWrapper color="purple" maxWidth="80rem">
+        <TankopediaPersistent.Provider>
+          <App.Provider>
+            <TankSearch />
+          </App.Provider>
+        </TankopediaPersistent.Provider>
+      </PageWrapper>
+    </LocaleProvider>
   );
 }
