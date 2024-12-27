844965bcb00a0d8faf8ff77e9aabcc57ec613935
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-27 12:55:52 -0600

    feat: locale redirect

--- packages/core/src/blitzkit/supportedLocales.ts
+++ packages/core/src/blitzkit/supportedLocales.ts
@@ -1,4 +1,5 @@
 export const SUPPORTED_LOCALES = ['en', 'es', 'hr', 'ru', 'uk', 'cn'];
+export const DEFAULT_LOCALE = SUPPORTED_LOCALES[0];
 export const LOCALE_FILES: Record<(typeof SUPPORTED_LOCALES)[number], string> =
   {
     en: 'en-US',
