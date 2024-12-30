599b81934ed69a234f0267fbb87728c85743c772
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-29 20:19:51 -0600

    feat: simplification

--- packages/bot/src/core/localization/strings/constants.ts
+++ packages/bot/src/core/localization/strings/constants.ts
@@ -1,6 +1,16 @@
-import { LOCALE_FILES } from '@blitzkit/core';
+import { SupportedLocale } from '@blitzkit/core';
 import { Locale } from 'discord.js';
 
+const DISCORD_SUPPORTED_LOCALES: Record<SupportedLocale, Locale> = {
+  en: Locale.EnglishUS,
+  es: Locale.SpanishES,
+  hr: Locale.Croatian,
+  pt: Locale.PortugueseBR,
+  ru: Locale.Russian,
+  uk: Locale.Ukrainian,
+  zh: Locale.ChineseCN,
+};
+
 export const SUPPORTED_DISCORD_LOCALES = Object.values(
-  LOCALE_FILES,
-) as Locale[];
+  DISCORD_SUPPORTED_LOCALES,
+);
--- packages/core/src/blitzkit/supportedLocales.ts
+++ packages/core/src/blitzkit/supportedLocales.ts
@@ -1,11 +1,13 @@
-export const SUPPORTED_LOCALES = ['en', 'es', 'hr', 'ru', 'uk', 'cn'];
+export const SUPPORTED_LOCALES = [
+  'en',
+  'es',
+  'hr',
+  'pt',
+  'ru',
+  'uk',
+  'zh',
+] as const;
+
 export const DEFAULT_LOCALE = SUPPORTED_LOCALES[0];
-export const LOCALE_FILES: Record<(typeof SUPPORTED_LOCALES)[number], string> =
-  {
-    en: 'en-US',
-    es: 'es-ES',
-    hr: 'hr',
-    ru: 'ru',
-    uk: 'uk',
-    cn: 'zh-CN',
-  };
+
+export type SupportedLocale = (typeof SUPPORTED_LOCALES)[number];
