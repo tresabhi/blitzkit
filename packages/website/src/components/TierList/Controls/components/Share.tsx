571ae81306caae6cdaf5f7f36a49f4cc3d31cc3e
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-01-07 13:09:05 -0600

    feat: translate tier list

--- packages/website/src/components/TierList/Controls/components/Share.tsx
+++ packages/website/src/components/TierList/Controls/components/Share.tsx
@@ -1,10 +1,12 @@
 import { Share2Icon } from '@radix-ui/react-icons';
 import { generateTierListParams } from '../../../../core/blitzkit/generateTierListParams';
+import { useLocale } from '../../../../hooks/useLocale';
 import { TierList } from '../../../../stores/tierList';
 import { CopyButton } from '../../../CopyButton';
 
 export function Share() {
   const rows = TierList.use((state) => state.rows);
+  const { strings } = useLocale();
 
   return (
     <CopyButton
@@ -13,7 +15,7 @@ export function Share() {
         `${location.origin}/tools/tier-list?${generateTierListParams(rows)}`
       }
     >
-      <Share2Icon /> Share
+      <Share2Icon /> {strings.website.tools.tier_list.buttons.share}
     </CopyButton>
   );
 }
--- packages/website/src/pages/[...locale]/tools/tier-list/_index.tsx
+++ packages/website/src/pages/[...locale]/tools/tier-list/_index.tsx
@@ -4,16 +4,22 @@ import { TierListControls } from '../../../../components/TierList/Controls';
 import { TierListTable } from '../../../../components/TierList/Table';
 import { TierListTiles } from '../../../../components/TierList/Tiles';
 import { URLManager } from '../../../../components/TierList/URLManager';
+import {
+  LocaleProvider,
+  type LocaleAcceptorProps,
+} from '../../../../hooks/useLocale';
 import { TankopediaPersistent } from '../../../../stores/tankopediaPersistent';
 import { TierList } from '../../../../stores/tierList';
 
-export function Page() {
+export function Page({ locale }: LocaleAcceptorProps) {
   return (
-    <TierList.Provider>
-      <TankopediaPersistent.Provider>
-        <Content />
-      </TankopediaPersistent.Provider>
-    </TierList.Provider>
+    <LocaleProvider locale={locale}>
+      <TierList.Provider>
+        <TankopediaPersistent.Provider>
+          <Content />
+        </TankopediaPersistent.Provider>
+      </TierList.Provider>
+    </LocaleProvider>
   );
 }
 
