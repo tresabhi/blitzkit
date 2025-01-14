ade6ec8af1b8b60dd64765c1d491f86d4852256f
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-01-13 20:06:43 -0600

    feat: localize tank search

--- packages/website/src/components/LinkI18n.tsx
+++ packages/website/src/components/LinkI18n.tsx
@@ -1,10 +1,12 @@
+import { DEFAULT_LOCALE } from '@blitzkit/core';
 import { Link, type LinkProps } from '@radix-ui/themes';
 import type { LocaleAcceptorProps } from '../hooks/useLocale';
 
 type LinkI18nProps = LinkProps & LocaleAcceptorProps;
 
 export function LinkI18n({ locale, href, ...props }: LinkI18nProps) {
-  const prefix = locale === undefined ? '' : `/${locale}`;
+  const prefix =
+    locale === undefined || locale === DEFAULT_LOCALE ? '' : `/${locale}`;
 
   return (
     <Link href={href?.startsWith('/') ? `${prefix}${href}` : href} {...props} />
