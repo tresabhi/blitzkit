7027f632366c6e9342a9b8ef3d524322533d6d29
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-01-17 19:56:46 -0600

    feat: prune

---
+++ packages/scripts/src/pruneStrings.ts
@@ -0,0 +1,49 @@
+import {
+  DEFAULT_LOCALE,
+  SUPPORTED_LOCALES,
+  TranslationTree,
+} from '@blitzkit/i18n';
+import { writeFile } from 'fs/promises';
+
+const ROOT = '@blitzkit/i18n/strings';
+const WORKING_ROOT = '../../packages/i18n/strings';
+
+const defaultStrings = await import(`${ROOT}/${DEFAULT_LOCALE}.json`);
+const nonDefaultLocales = SUPPORTED_LOCALES.filter(
+  (locale) => locale !== DEFAULT_LOCALE,
+);
+
+function prune(defaultStrings: TranslationTree, strings: TranslationTree) {
+  for (const key in strings) {
+    if (key in defaultStrings) {
+      if (typeof defaultStrings[key] === typeof strings[key]) {
+        if (typeof strings[key] === 'string') {
+          if (
+            (defaultStrings[key] as string).replaceAll(/%s\d/g, '%s') ===
+              (strings[key] as string).replaceAll(/%s\d/g, '%s') ||
+            strings[key] === ''
+          ) {
+            delete strings[key];
+          }
+        } else {
+          prune(defaultStrings[key] as TranslationTree, strings[key]);
+        }
+      } else {
+        delete strings[key];
+      }
+    } else {
+      delete strings[key];
+    }
+  }
+
+  return strings;
+}
+
+for (const locale of nonDefaultLocales) {
+  console.log(`Pruning ${locale}...`);
+
+  const strings = await import(`${ROOT}/${locale}.json`);
+  const pruned = prune(defaultStrings, strings);
+
+  writeFile(`${WORKING_ROOT}/${locale}.json`, JSON.stringify(pruned, null, 2));
+}
