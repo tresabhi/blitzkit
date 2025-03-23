da4c2ece322972c41fd1ceee006821b50afac521
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-03-23 17:51:56 -0500

    feat: better starting angle

--- packages/website/src/core/blitzkit/dissect.ts
+++ packages/website/src/core/blitzkit/dissect.ts
@@ -1,4 +1,5 @@
 import { EventManager } from '@blitzkit/core';
+import { degToRad } from 'three/src/math/MathUtils.js';
 
 export interface DissectEventData {
   rotation: number;
@@ -8,8 +9,8 @@ export interface DissectEventData {
 export const dissectEvent = new EventManager<DissectEventData>();
 
 export const lastDissection: DissectEventData = {
-  rotation: Math.PI / 2,
-  offset: 0,
+  rotation: degToRad(100),
+  offset: 0.8,
 };
 
 dissectEvent.on((data) => Object.assign(lastDissection, data));
