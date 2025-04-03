c3d8f15a2a8187d2a5376bd0b6c3ea01e19930ff
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-04-03 12:23:53 -0500

    feat: better robots.txt

--- packages/website/src/pages/robots.txt.ts
+++ packages/website/src/pages/robots.txt.ts
@@ -1,23 +1,19 @@
 import { assertSecret } from '@blitzkit/core';
 import type { APIContext } from 'astro';
 
-export async function GET({}: APIContext) {
+export async function GET({ request }: APIContext) {
   const allowRobots = assertSecret(import.meta.env.ALLOW_ROBOTS) === 'true';
-  const site = assertSecret(import.meta.env.SITE);
+  const flags: Record<string, string | null> = {
+    'User-agent': '*',
+    [allowRobots ? 'Allow' : 'Disallow']: '/',
+
+    space: null,
+    Sitemap: `${new URL(request.url).origin}/sitemap-index.xml`,
+  };
 
   return new Response(
-    `
-User-agent: *
-Allow: /
-
-${
-  allowRobots
-    ? `
-Host: ${site}
-Sitemap: ${site}/sitemap-index.xml
-`
-    : ''
-}
-`.trim(),
+    Object.entries(flags)
+      .map(([key, value]) => (value === null ? '' : `${key}: ${value}`))
+      .join('\n'),
   );
 }
