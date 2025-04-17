e20d65d218dc660501ad2c285eefb99800b2b158
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-04-16 23:22:12 -0500

    feat: darker

--- packages/website/src/components/Tankopedia/HeroSection/components/TankSandbox/components/TransitionSkeleton/shaders/fragment.glsl
+++ packages/website/src/components/Tankopedia/HeroSection/components/TankSandbox/components/TransitionSkeleton/shaders/fragment.glsl
@@ -4,6 +4,6 @@
 
 void main() {
   float fogFactor = smoothstep(fogNear, fogFar, vFogDepth);
-  gl_FragColor = vec4(0.75, 0.75, 0.75, fogFactor);
+  gl_FragColor = vec4(0.5, 0.5, 0.5, fogFactor);
   gl_FragColor.rgb = mix(gl_FragColor.rgb, fogColor, 1.0 - fogFactor);
 }
