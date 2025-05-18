2c7082fe04ffb67bff3f13e9f323ef1070c9c6e3
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-05-18 09:54:12 -0500

    feat: fast assets

--- packages/core/src/blitzkit/asset.ts
+++ packages/core/src/blitzkit/asset.ts
@@ -1,7 +1,12 @@
 import { assertSecret } from './assertSecret';
 
+const repo = assertSecret(import.meta.env.PUBLIC_ASSET_REPO);
+const branch = assertSecret(import.meta.env.PUBLIC_ASSET_BRANCH);
+const fastUser = assertSecret(import.meta.env.PUBLIC_ASSET_FAST_USER);
+const fastRepo = assertSecret(import.meta.env.PUBLIC_ASSET_FAST_REPO);
+
 export function asset(path: string) {
-  return `https://raw.githubusercontent.com/${assertSecret(
-    import.meta.env.PUBLIC_ASSET_REPO,
-  )}/${assertSecret(import.meta.env.PUBLIC_ASSET_BRANCH)}/${path}`;
+  return branch === 'main'
+    ? `https://${fastUser}.github.io/${fastRepo}/${path}`
+    : `https://raw.githubusercontent.com/${repo}/${branch}/${path}`;
 }
